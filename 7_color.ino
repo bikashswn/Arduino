9efaecad81a4ea2651bdda09918287e7a02f145a
bikashswn <bikashswn@gmail.com>
2018-02-13 14:01:23 +0530

    7 color led blink

---
+++ 7_color.ino
@@ -0,0 +1,14 @@
+int Led = 7;
+ 
+void setup ()
+{
+  pinMode (Led, OUTPUT); // Initialization of the LED output pin
+}
+ 
+void loop () // main program loop
+{
+  digitalWrite (Led, HIGH); // LED will be switched on
+  delay (4000); // waitmode for 4 seconds
+  digitalWrite (Led, LOW); // LED will be switched off
+  delay (2000); // waitmode for another 2 seconds
+}
